2ca9117cbe083f32c19d42dfa08b20b45f29369a
GitAmina <151671847+GitHunAmina@users.noreply.github.com>
2024-01-12 19:32:44 +0000

    Documentation

--- src/main/java/com/aminata/gestionstagiaire/GestionstagiaireApplication.java
+++ src/main/java/com/aminata/gestionstagiaire/GestionstagiaireApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
-
 @SpringBootApplication
-
 public class GestionstagiaireApplication implements CommandLineRunner {
 
 	@Autowired
